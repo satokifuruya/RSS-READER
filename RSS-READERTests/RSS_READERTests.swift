3e8044e31b80cc9e5f3ed8210d3f868d65fa0f09
satokifuruya <satokifuruya@gmail.com>
2016-08-22 16:07:24 +0900

    Initial Commit

---
+++ RSS-READERTests/RSS_READERTests.swift
@@ -0,0 +1,36 @@
+//
+//  RSS_READERTests.swift
+//  RSS-READERTests
+//
+//  Created by FS on 2016/08/22.
+//  Copyright © 2016年 shigeru arayama. All rights reserved.
+//
+
+import XCTest
+@testable import RSS_READER
+
+class RSS_READERTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
